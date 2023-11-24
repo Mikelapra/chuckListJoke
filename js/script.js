53a7a883408589059ea4d0efd36649771aa9e963
Mikel Apraiz <apraiz.mikel@gmail.com>
2023-11-25 00:13:53 +0100

    Entregable Sprint 5 Team Challenge

--- js/script.js
+++ js/script.js
@@ -1,33 +1,32 @@
 const jokeList = document.getElementById("jokeList");
 const fetchJoke = document.getElementById("fetchJoke");
-
 const registroChiste = JSON.parse(localStorage.getItem("registroChiste")) || {}
-
 let contador = localStorage.getItem("contador") || 0
-
 fetchJoke.addEventListener("click", ()=>{
     fetch("https://api.chucknorris.io/jokes/random")
     .then((respuesta) => {
         if (!respuesta.ok) {
             throw new Error ("Error");
         }
-        return respuesta.json() 
+        return respuesta.json()
     })
     .then ((data) => {
     contador ++;
     localStorage.setItem("contador", contador)
     registroChiste[`chiste${contador}`] = data.value
     localStorage.setItem("registroChiste", JSON.stringify(registroChiste))
-    jokeList.innerHTML += `<li>${data.value}`
+    jokeList.innerHTML = ""
     printuser()
 })
 })
+printuser()
 
 function printuser () {
     const chiste = JSON.parse(localStorage.getItem("registroChiste"));
     const claveChistes = Object.keys(chiste)
     claveChistes.forEach ((llave) => {
-        jokeList.innerHMTL += `<li>${claveChistes[llave]}</li>`
+        console.log(chiste[llave])
+        console.log(jokeList.innerHTML)
+        jokeList.innerHTML += `<li>${chiste[llave]}</li>`
     })
 }
-// console.log(jokeList)
